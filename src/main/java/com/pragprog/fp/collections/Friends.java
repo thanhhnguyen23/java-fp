82ecef799225f522d40931e193473f0e788ba1b6
Thanh Nguyen <thanh.h.nguyen23@gmail.com>
2020-01-06 16:38:47 -0800

    add friends' list

---
+++ src/main/java/com/pragprog/fp/collections/Friends.java
@@ -0,0 +1,11 @@
+package com.pragprog.fp.collections;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Friends {
+    public List<String> retrieveList() {
+        List<String> friends = Arrays.asList("Brian", "Nate", "Neal", "Raju", "Sara", "Scott");
+        return friends;
+    }
+}
